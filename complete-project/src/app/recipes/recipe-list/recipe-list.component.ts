fb2af64c59fcb903fa51bcd7ac7c408b6b748602
Deadfaced <btteixeira.bt@gmail.com>
2024-03-07 14:39:36 +0000

    project's first section complete

---
+++ complete-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('New Recipe 1', 'First test recipe', 'https://picsum.photos/300/200'),
+    new Recipe('New Recipe 2', 'Second test recipe', 'https://picsum.photos/300/200'),
+    new Recipe('New Recipe 3', 'Third test recipe', 'https://picsum.photos/300/200'),
+  ];
+
+}
---
+++ complete-project/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrl: './shopping-list.component.css'
+})
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Strawberries', 17),
+    new Ingredient('Bananas', 12),
+    new Ingredient('Pineapples', 2),
+  ];
+}
