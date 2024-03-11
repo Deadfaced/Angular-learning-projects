9f6ab86ae354ebf70fbf63b009e8911e7c65313c
Deadfaced <btteixeira.bt@gmail.com>
2024-03-11 16:30:21 +0000

    finished initial setup and socials component

--- Personal projects/jpPractice/src/app/app.module.ts
+++ Personal projects/jpPractice/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { SocialsComponent } from './socials/socials.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    SocialsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
