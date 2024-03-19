e1ba765b20a9521bc13d2ad30b7267262da63ad0
Deadfaced <btteixeira.bt@gmail.com>
2024-03-19 17:47:00 +0000

    made social links responsive

--- personalProjects/jpPractice/src/app/socials/socials.component.ts
+++ personalProjects/jpPractice/src/app/socials/socials.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-socials',
   templateUrl: './socials.component.html',
   styleUrl: './socials.component.css'
 })
-export class SocialsComponent {
+export class SocialsComponent implements OnInit{
+  mobile = false;
+  
+  constructor(private responsive: BreakpointObserver){}
 
+  ngOnInit(): void {
+    this.responsive.observe(Breakpoints.HandsetPortrait)
+      .subscribe(result => {
+        this.mobile = false;
+
+        if(result.matches){
+          this.mobile = true;
+        }
+      })
+  }
 }
